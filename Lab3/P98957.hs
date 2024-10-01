a68a221dc42e90ec4c274666d4249be5d4de7495
Guido Biosca Lasa <guido.biosca@a5s102pc23.fib.upc.es>
2024-10-01 11:47:20 +0200

    ns

--- Lab3/P98957.hs
+++ Lab3/P98957.hs
@@ -19,3 +19,10 @@ triangulars = map aux [0..]
 factorials :: [Integer]
 factorials = map aux [0..]
     where aux num = foldl (*) 1 [1..num]
+
+{-
+Hacer la funcion merge que es para que juntar dos listas y ordenarlas
+-}
+
+fibs :: [Integer]
+fibs = map aux iterate (+1) 0
